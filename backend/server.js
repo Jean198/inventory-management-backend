785f7fc1a7588396874406d5811d53aeeb2ef7f3
Jean198 <jeandedieu.niyigaba@gmail.com>
2023-02-07 09:27:56 +0200

    Created user controller and route

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const dotenv = require("dotenv").config();
+const express = require('express');
+const dotenv = require('dotenv').config();
 const app = express();
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
-const userRoute = require("./routes/userRoute");
-const errorHandler = require("../backend/middlewares/errorMiddleware");
-mongoose.set("strictQuery", true);
+const userRoute = require('./routes/userRoute');
+const errorHandler = require('../backend/middlewares/errorMiddleware');
+mongoose.set('strictQuery', true);
 
 //Middlewares
 app.use(express.json());
@@ -15,11 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Routes middleware
-app.use("/api/users", userRoute);
-
-app.get("/", (req, res) => {
-  res.send("Homepage");
-});
+app.use('/api/users', userRoute);
 
 //Error middleware
 app.use(errorHandler);
